d751f420ab184882a7e6a21e4577cb35675a6e34
Zientak.David <Zenakz@github.com>
2020-06-08 18:32:04 +0200

    added water state test

--- testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
@@ -37,14 +37,26 @@ class CoffeeMachineTest {
 
     @Test
     void properMilklessOrderShouldResultInMilklessCoffee(){
-        CoffeeSize unrelevant = CoffeeSize.DOUBLE;
+        CoffeeSize size = CoffeeSize.DOUBLE;
         CoffeType anyType = CoffeType.CAPUCCINO;
-        CoffeOrder coffeOrder = CoffeOrder.builder().withSize(unrelevant).withType(anyType).build();
-        setFunctions(unrelevant,anyType);
+        CoffeOrder coffeOrder = CoffeOrder.builder().withSize(size).withType(anyType).build();
+        setFunctions(size,anyType);
 
         Coffee result = coffeeMachine.make(coffeOrder);
 
         assertEquals(0,result.getMilkAmout());
+    }
+
+    @Test
+    void properOrderShouldHaveCorrectWater(){
+        CoffeeSize size = CoffeeSize.DOUBLE;
+        CoffeType anyType = CoffeType.CAPUCCINO;
+        CoffeOrder coffeOrder = CoffeOrder.builder().withSize(size).withType(anyType).build();
+        setFunctions(size,anyType);
+
+        Coffee result = coffeeMachine.make(coffeOrder);
+
+        assertEquals(10,result.getWaterAmount());
     }
 
     @Test
@@ -55,35 +67,33 @@ class CoffeeMachineTest {
 
     @Test
     void properOrderShouldCallGrinderAndReceipes(){
-        CoffeeSize unrelevant = CoffeeSize.DOUBLE;
+        CoffeeSize size = CoffeeSize.DOUBLE;
         CoffeType anyType = CoffeType.CAPUCCINO;
-        CoffeOrder coffeOrder = CoffeOrder.builder().withSize(unrelevant).withType(anyType).build();
-        setFunctions(unrelevant, anyType);
+        CoffeOrder coffeOrder = CoffeOrder.builder().withSize(size).withType(anyType).build();
+        setFunctions(size, anyType);
         Coffee result = coffeeMachine.make(coffeOrder);
 
-        verify(grinder).canGrindFor(unrelevant);
+        verify(grinder).canGrindFor(size);
         verify(coffeeReceipes, times(2)).getReceipe(anyType);
     }
 
     @Test
     void ungrindableSizeShouldReturnInError(){
-        CoffeeSize unrelevant = CoffeeSize.DOUBLE;
+        CoffeeSize size = CoffeeSize.DOUBLE;
         CoffeType anyType = CoffeType.CAPUCCINO;
-        CoffeOrder coffeOrder = CoffeOrder.builder().withSize(unrelevant).withType(anyType).build();
-        when(grinder.canGrindFor(unrelevant)).thenReturn(false);
+        CoffeOrder coffeOrder = CoffeOrder.builder().withSize(size).withType(anyType).build();
+        when(grinder.canGrindFor(size)).thenReturn(false);
         assertThrows(NoCoffeeBeansException.class, ()->{
             coffeeMachine.make(coffeOrder);
         });
     }
 
     private void setFunctions(CoffeeSize size, CoffeType type){
-        CoffeeSize unrelevant = CoffeeSize.DOUBLE;
-        CoffeType anyType = CoffeType.CAPUCCINO;
-        CoffeOrder coffeOrder = CoffeOrder.builder().withSize(unrelevant).withType(anyType).build();
-        when(grinder.canGrindFor(unrelevant)).thenReturn(true);
-        when(grinder.grind(unrelevant)).thenReturn(5d);
+        CoffeOrder coffeOrder = CoffeOrder.builder().withSize(size).withType(type).build();
+        when(grinder.canGrindFor(size)).thenReturn(true);
+        when(grinder.grind(size)).thenReturn(5d);
         CoffeeReceipe receipe = CoffeeReceipe.builder().withMilkAmount(0).withWaterAmounts(waterAmounts).build();
-        when(coffeeReceipes.getReceipe(anyType)).thenReturn(receipe);
+        when(coffeeReceipes.getReceipe(type)).thenReturn(receipe);
     }
 
 }
