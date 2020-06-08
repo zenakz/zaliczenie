8083af8175b2ee87056260e90a0a309a32703389
Zientak.David <Zenakz@github.com>
2020-06-08 18:39:07 +0200

    small fixes

--- testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
@@ -16,57 +16,48 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.HashMap;
 import java.util.Map;
 
-@ExtendWith(MockitoExtension.class)
-class CoffeeMachineTest {
+@ExtendWith(MockitoExtension.class) class CoffeeMachineTest {
 
-    @Mock
-    private Grinder grinder;
-    @Mock
-    private MilkProvider milkProvider;
-    @Mock
-    private CoffeeReceipes coffeeReceipes;
+    @Mock private Grinder grinder;
+    @Mock private MilkProvider milkProvider;
+    @Mock private CoffeeReceipes coffeeReceipes;
 
     private CoffeeMachine coffeeMachine;
 
-    private Map<CoffeeSize, Integer>waterAmounts = new HashMap<>();
-    @BeforeEach
-    void setUp(){
-        coffeeMachine = new CoffeeMachine(grinder,milkProvider,coffeeReceipes);
-        waterAmounts.put(CoffeeSize.DOUBLE,10);
+    private Map<CoffeeSize, Integer> waterAmounts = new HashMap<>();
+
+    @BeforeEach void setUp() {
+        coffeeMachine = new CoffeeMachine(grinder, milkProvider, coffeeReceipes);
+        waterAmounts.put(CoffeeSize.DOUBLE, 10);
     }
 
-    @Test
-    void properMilklessOrderShouldResultInMilklessCoffee(){
+    @Test void properMilklessOrderShouldResultInMilklessCoffee() {
         CoffeeSize size = CoffeeSize.DOUBLE;
         CoffeType anyType = CoffeType.CAPUCCINO;
         CoffeOrder coffeOrder = CoffeOrder.builder().withSize(size).withType(anyType).build();
-        setFunctions(size,anyType);
+        setFunctions(size, anyType);
 
         Coffee result = coffeeMachine.make(coffeOrder);
 
-        assertEquals(0,result.getMilkAmout());
+        assertEquals(0, result.getMilkAmout());
     }
 
-    @Test
-    void properOrderShouldHaveCorrectWater(){
+    @Test void properOrderShouldHaveCorrectWater() {
         CoffeeSize size = CoffeeSize.DOUBLE;
         CoffeType anyType = CoffeType.CAPUCCINO;
         CoffeOrder coffeOrder = CoffeOrder.builder().withSize(size).withType(anyType).build();
-        setFunctions(size,anyType);
+        setFunctions(size, anyType);
 
         Coffee result = coffeeMachine.make(coffeOrder);
 
-        assertEquals(10,result.getWaterAmount());
+        assertEquals(10, result.getWaterAmount());
     }
 
-    @Test
-    public void itCompiles() {
+    @Test public void itCompiles() {
         assertThat(true, Matchers.equalTo(true));
     }
 
-
-    @Test
-    void properOrderShouldCallGrinderAndReceipes(){
+    @Test void properOrderShouldCallGrinderAndReceipes() {
         CoffeeSize size = CoffeeSize.DOUBLE;
         CoffeType anyType = CoffeType.CAPUCCINO;
         CoffeOrder coffeOrder = CoffeOrder.builder().withSize(size).withType(anyType).build();
@@ -77,19 +68,19 @@ class CoffeeMachineTest {
         verify(coffeeReceipes, times(2)).getReceipe(anyType);
     }
 
-    @Test
-    void ungrindableSizeShouldReturnInError(){
+    @Test void ungrindableSizeShouldReturnError() {
         CoffeeSize size = CoffeeSize.DOUBLE;
         CoffeType anyType = CoffeType.CAPUCCINO;
         CoffeOrder coffeOrder = CoffeOrder.builder().withSize(size).withType(anyType).build();
         when(grinder.canGrindFor(size)).thenReturn(false);
-        assertThrows(NoCoffeeBeansException.class, ()->{
+
+        assertThrows(NoCoffeeBeansException.class, () -> {
             coffeeMachine.make(coffeOrder);
         });
     }
 
-    private void setFunctions(CoffeeSize size, CoffeType type){
-        CoffeOrder coffeOrder = CoffeOrder.builder().withSize(size).withType(type).build();
+
+    private void setFunctions(CoffeeSize size, CoffeType type) {
         when(grinder.canGrindFor(size)).thenReturn(true);
         when(grinder.grind(size)).thenReturn(5d);
         CoffeeReceipe receipe = CoffeeReceipe.builder().withMilkAmount(0).withWaterAmounts(waterAmounts).build();
